e59054183f3396d4aaf34159e43c7648914e53e9
Jeff Hoover <jeffhoover@ameritech.net>
2011-01-30 17:02:57 -0500

    enabled serve_static_assets for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,10 @@ SampleApp::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+#  config.serve_static_assets = false
+  # Put it back so that 'git heroku push' doesn't show warning.
+  # See - http://docs.heroku.com/rails3
+  config.serve_static_assets = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
