656b1a64fbdccc859f834c4b2a8a374e979bfd59
Jeff Hoover <jeffhoover@ameritech.net>
2011-01-30 16:58:29 -0500

    Finished layout and routes

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe "LayoutLinks" do
 
+  it "should have a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+  end
+
   it "should have a Home page at '/'" do
     get '/'
     response.should have_selector('title', :content => "Home")
