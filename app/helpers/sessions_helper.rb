e3387d754e7c4b6ea33a5a132bae511d78f0b28a
Jeff Hoover <jeffhoover@ameritech.net>
2011-02-06 20:31:53 -0500

    done with sign in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,11 @@ module SessionsHelper
     self.current_user = user
   end
 
+  def sign_out
+    cookies.delete(:remember_token)
+    self.current_user = nil
+  end
+
   def current_user
     @current_user ||= user_from_remember_token
   end
